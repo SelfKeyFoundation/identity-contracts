dcb0ba0308bc7fc92970eaa84b90251d801585b2
Carlos Bruguera <carlos@selfkey.org>
2018-03-20 09:44:43 +0700

    Added linting. Separated identity interface (erc725-like) from implementation

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.19;
 
+import './lib/ERC725b.sol';
+
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 import 'zeppelin-solidity/contracts/lifecycle/Destructible.sol';
 
@@ -7,39 +9,29 @@ import 'zeppelin-solidity/contracts/lifecycle/Destructible.sol';
  * @title Identity
  * @dev Contract that represents an identity
  */
-contract Identity is Ownable, Destructible {
+contract Identity is ERC725b, Ownable, Destructible {
     bytes16 idVersion = "0.0.1";
-
-    // TODO: Add constants for key schemes
-
-    // TODO: move to an interface
-    uint256 constant MANAGEMENT_KEY = 1;
-    uint256 constant ACTION_KEY = 2;
-    uint256 constant CLAIM_SIGNER_KEY = 3;
-    uint256 constant ENCRYPTION_KEY = 4;
-
-    struct Key {
-        address key;    //ERC725 defines it as bytes32
-        uint256 keyType;
-        uint256 scheme;
-    }
-
-    // maps from type to keys
-    mapping(bytes32 => Key) keys;
-
-    event KeyAdded(address indexed key, uint256 indexed keyType);
-    event KeyRemoved(address indexed key, uint256 indexed keyType);
 
     modifier onlyManager () {
         require(keys[keccak256(msg.sender, MANAGEMENT_KEY)].key != 0);
         _;
     }
 
+    /**
+     * @dev Identity Constructor. Assigns a Management key to the creator.
+     * @param _sender — The creator of this identity.
+     */
     function Identity(address sender) public {
         // Adds sender as a management key
-        keys[keccak256(sender, MANAGEMENT_KEY)] = Key(sender, MANAGEMENT_KEY, 1);   // TODO: set proper scheme argument
+        keys[keccak256(sender, MANAGEMENT_KEY)] = Key(sender, MANAGEMENT_KEY, ECDSA);
     }
 
+    /**
+     * @dev Adds a new key (address) to the identity
+     * @param _key — The key (address) to be added
+     * @param _type — The key type (Management, Action, etc)
+     * @param _scheme — The scheme to be used for verifying this key (ECDSA, RSA, etc)
+     */
     function addKey(address _key, uint256 _type, uint256 _scheme)
         onlyManager
         public
@@ -51,6 +43,11 @@ contract Identity is Ownable, Destructible {
         return true;
     }
 
+    /**
+     * @dev Retrieves a key by its address and type
+     * @param _key — The key (address) to be retrieved
+     * @param _type — The key type (Management, Action, etc)
+     */
     function getKey(address _key, uint256 _type)
         public
         view
@@ -62,6 +59,11 @@ contract Identity is Ownable, Destructible {
         return (keys[index].key, keys[index].keyType, keys[index].scheme);
     }
 
+    /**
+     * @dev Removes a key (doable only by addresses added of the MANAGEMENT type)
+     * @param _key — The key (address) to be removed
+     * @param _type — The key type (Management, Action, etc)
+     */
     function removeKey(address _key, uint256 _type)
         onlyManager
         public
@@ -69,7 +71,7 @@ contract Identity is Ownable, Destructible {
     {
         bytes32 index = keccak256(_key, _type);
         delete keys[index];
-        
+
         KeyRemoved(_key, _type);
 
         return true;
